86278179d5c4ef947751dd56836703786f8fd3df
Damini-nayak <100026489+Damini-nayak@users.noreply.github.com>
2025-03-18 20:31:05 +0530

    create react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='quiz-container'>
+        <div className='quizzes'>
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
